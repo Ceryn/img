9aec432ba85909860793451ff53fc9dc0790d420
Ceryn <git@ceryn.me>
2014-02-20 13:13:58 +0100

    Made selections optional--full screen is now the default.

--- img.sh
+++ img.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # https://github.com/Ceryn/img
+# Call with '-s' to target only a selection of the screen.
 
 clientid='3e7a4deb7ac67da'
 img=$(date '+/tmp/%N.png')
 
-scrot -zs $img >/dev/null 2>&1 || exit
+scrot -z "$@" $img >/dev/null 2>&1 || exit
 res=$(curl -sH "Authorization: Client-ID $clientid" -F "image=@$img" "https://api.imgur.com/3/upload")
 
-echo $res | egrep -qo '"status":200' && link=$(echo $res | sed -e 's/.*"link":"\([^"]*\).*/\1/' -e 's/\\//g')
+echo $res | grep -qo '"status":200' && link=$(echo $res | sed -e 's/.*"link":"\([^"]*\).*/\1/' -e 's/\\//g')
 test -n "$link" && (printf $link | xclip; printf "\a") || echo "$res" > "$img.error"
