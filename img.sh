afbf8583bc6def5dd27975f5a33c1c3bb4e2c251
Daniel Schep <dschep@gmail.com>
2014-10-23 15:45:25 -0400

    Use mktemp instead of date for safety

--- img.sh
+++ img.sh
@@ -3,7 +3,7 @@
 # Call with '-s' to target only a selection of the screen.
 
 clientid='3e7a4deb7ac67da'
-img=$(date '+/tmp/%N.png')
+img=$(mktemp '/tmp/img-XXXXXX.png')
 
 scrot -z "$@" $img >/dev/null 2>&1 || exit
 res=$(curl -sH "Authorization: Client-ID $clientid" -F "image=@$img" "https://api.imgur.com/3/upload")
